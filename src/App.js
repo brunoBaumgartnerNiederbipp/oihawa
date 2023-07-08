6085b2c2aec0d57d83a6978ee464fb67a79c993c
bruno baumgartner <leon@leuthold39@gmail.com>
2023-07-09 01:50:14 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Comming Soon!</h1>
+        <h2>Bruno Baumgartner</h2>
+      <h2>Webdesign Niederbipp</h2> 
+      <p>079 955 28 59</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
